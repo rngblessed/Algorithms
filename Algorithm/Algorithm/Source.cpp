7c22cc1b3447f68f57ecacfc90c858a6ec2ed589
rngblessed <revizen@yandex.ru>
2023-09-19 21:19:14 +0300

    trying to upload Algo lab

--- Algorithm/Algorithm/Source.cpp
+++ Algorithm/Algorithm/Source.cpp
@@ -1,19 +1,16 @@
 #include <algorithm>
 #include <iostream>
-#include <fstream>
 #include <ctime>
 
 int bubbleSort(int*& a, int n) {
 	int cnt = 0;
-	for (int i = 0; i < n - 1; ++i) {
-		cnt++;
+	srand(time(0));
+	for (int i = 0; i < n; ++i) {
 		for (int j = i + 1; j < n; ++j) {
-			cnt++;
+			cnt += 2;
 			if (a[i] < a[j]) {
-				int temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-				cnt += 3;
+				std::swap(a[i], a[j]);
+				cnt += 5;
 			}
 		}
 	}
@@ -22,53 +19,54 @@ int bubbleSort(int*& a, int n) {
 
 int** makeArray(int option) {
 	int** a = new int* [10];
-		for (int i = 0; i < 10; ++i) {
-			a[i] = new int[(i + 1) * 100];
-			for (int j = 0; j <= (i + 1) * 100; ++j) {
-				switch (option) {
-					case(1): {
-						a[i][j] = 1;
-						break;
-					}
-					case(2): {
-						srand(time(0));
-						a[i][j] = rand() % 1000;
-						break;
-					}
-					case(3): {
-						a[i][j] = j;
-						break;
-					}
-				}
+	for (int i = 0; i < 10; ++i) {
+		a[i] = new int[(i + 1) * 100];
+		for (int j = 0; j < (i + 1) * 100; ++j) {
+			switch (option) {
+			case(1): {
+				a[i][j] = 1;
+				break;
+			}
+			case(2): {
+				a[i][j] = std::rand() % 1000;
+				break;
+			}
+			case(3): {
+				a[i][j] = j + 1;
+				break;
+			}
 			}
 		}
+	}
 	return a;
-}
-
-void printArray(int* a, int n) {
-	for (int i = 0; i < n; ++i)
-		std::cout << a[i] << ' ';
 }
 
 int main() {
 	int** minArray;
 	int** maxArray;
-	int** avgArray;
+	int** meanArray;
 	minArray = makeArray(1);
 	maxArray = makeArray(3);
-	avgArray = makeArray(2);
-	int** result = new int*[3];
+	meanArray = makeArray(2);
+	int** result = new int* [3];
 	for (int i = 0; i < 3; ++i)
-	result[i] = new int[10];
+		result[i] = new int[10];
 	for (int j = 0; j < 10; ++j) {
-		result[0][j] = bubbleSort(*minArray, (j + 1) * 100);
-		result[1][j] = bubbleSort(*avgArray, (j + 1) * 100);
-		result[2][j] = bubbleSort(*maxArray, (j + 1) * 100);
+		int sum = 0;
+		result[0][j] = bubbleSort(*&minArray[j], (j + 1) * 100);
+		for (int n = 0; n < 1000; ++n) {
+			sum += bubbleSort(*&meanArray[j], (j + 1) * 100);
+			meanArray = makeArray(2);
+		}
+		result[1][j] = sum / 1000;
+		result[2][j] = bubbleSort(*&maxArray[j], (j + 1) * 100);
 	}
-	for(int i = 0; i < 10; ++i)
-		std::cout << "minimal difficulty for n = " << (i + 1) * 100 << " is " << result[0][i] << "\n";
 	for (int i = 0; i < 10; ++i)
-		std::cout << "average difficulty for n = " << (i + 1) * 100 << " is " << result[1][i] << "\n";
+		std::cout << "minimal difficulty for n = " << (i + 1) * 100 << " is " << result[0][i] << "\n";
+	std::cout << "\n";
+	for (int i = 0; i < 10; ++i)
+		std::cout << "mean difficulty for n = " << (i + 1) * 100 << " is " << result[1][i] << "\n";
+	std::cout << "\n";
 	for (int i = 0; i < 10; ++i)
 		std::cout << "maximum difficulty for n = " << (i + 1) * 100 << " is " << result[2][i] << "\n";
 }
